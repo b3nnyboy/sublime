a760f9fa146e332704a153e19ed17ad12b3aaa0e
b3nnyboy <whythehell10152007@gmail.com>
2020-01-24 17:08:48 +0800

    first push

---
+++ lesson1/app.js
@@ -0,0 +1,11 @@
+$('#light').click(function(){
+	$('body').css('background-color',"white");
+	$('.boxes').css('background-color',"PeachPuff")
+	$('body').css('color', "navy");
+})
+
+$('#dark').click(function(){
+	$('body').css('background-color',"black");
+	$('.boxes').css('background-color',"grey")
+	$('body').css('color', "white");
+}
